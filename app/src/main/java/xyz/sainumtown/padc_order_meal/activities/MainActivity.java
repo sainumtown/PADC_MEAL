75d1e287066465aaa2096cdb71118a0d2d2777fe
Sai Num Town <sainumtown@gmail.com>
2016-08-27 09:19:25 +0630

    Changed Navigatrion drawer

--- app/src/main/java/xyz/sainumtown/padc_order_meal/activities/MainActivity.java
+++ app/src/main/java/xyz/sainumtown/padc_order_meal/activities/MainActivity.java
@@ -12,7 +12,6 @@ import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -113,18 +112,8 @@ public class MainActivity extends BaseActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_mealList) {
             navigateToMeal();
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
